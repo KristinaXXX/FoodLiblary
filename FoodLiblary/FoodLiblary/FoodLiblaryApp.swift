84456d433a2faf19a5bbf709e5ff4f6768f95a09
KristinaXXX <107407324+KristinaXXX@users.noreply.github.com>
2024-01-06 22:39:11 +0500

    init

---
+++ FoodLiblary/FoodLiblary/FoodLiblaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodLiblaryApp.swift
+//  FoodLiblary
+//
+//  Created by Kr Qqq on 06.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodLiblaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
